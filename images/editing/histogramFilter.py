b3c5bbba66e2fe25b9aeab3988e880bfa7bcc02f
purvitrivedi <purvitriv@gmail.com>
2020-06-05 16:50:57 +0100

    fixed matplot issue

--- images/editing/histogramFilter.py
+++ images/editing/histogramFilter.py
@@ -1,15 +1,6 @@
-import requests
-import numpy as np
-import base64
-import urllib
 from PIL import Image, ImageChops
-import matplotlib.pyplot as plt
-
 from skimage import io
 from skimage.exposure import match_histograms
-from skimage import filters, segmentation, color
-from skimage.util import compare_images
-from skimage.color import rgb2gray
 
 output_filename = 'histogramTest'
 
